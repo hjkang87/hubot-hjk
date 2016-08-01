202fc869f897c3bd83b6403efa6677f0300a4398
Hyunjung Kang <kanghyunjung@gmail.com>
2016-08-01 13:13:27 +0900

    add findExample
    Signed-off-by: Hyunjung Kang <kanghyunjung@gmail.com>

--- scripts/wordScrape.js
+++ scripts/wordScrape.js
@@ -1,10 +1,10 @@
 var jsdom = require('jsdom');
-var url = 'http://m.wordbook.naver.com/endic/today/words.nhn?targetDate=';
-var selectors = ['.words', '.txt_ct2'];
 
 module.exports = function(robot) {
 
     scrapeWords = function(msg) {
+
+        var url = 'http://m.wordbook.naver.com/endic/today/words.nhn?targetDate=';
 
         jsdom.env(url, function(err, window) {
             var $ = require('jquery')(window);
@@ -37,9 +37,57 @@ module.exports = function(robot) {
         });
     }
 
+    findExample = function(msg) {
+        word = msg.match[1];
+
+        if(msg.match.length>2 && parseInt(msg.match[2])) {
+            num = msg.match[2];
+        } else {
+            num = 5;
+        }
+
+        url = 'http://endic.naver.com/search_example.nhn?sLn=kr&query='+ encodeURIComponent(word) + '&preQuery=&searchOption=example&forceRedirect=N';
+        console.log(url);
+
+        jsdom.env(url, function(err, window) {
+            var $ = require('jquery')(window);
+
+            if(err) {
+                console.log(err);
+            } else {
+                sentences = [];
+                translations= [];
+
+                window.$(".list_a_mar .mar_top01").each(function() {
+                    tmp = "";
+                    $(this).find("i").each(function() {
+                        tmp+= $(this).text() + " ";
+                    });
+                    sentences.push(tmp);
+                });
+
+                window.$("[class='N=a:xmp.detail']:not(.detail_url_link)").each(function() {
+                    translations.push($(this).text());
+                });
+            }
+
+            results = "";
+            for(i=0; i<Math.min(num, sentences.length); i++) {
+                results+= sentences[i] + "\n";
+                results+= translations[i] + "\n";
+            }
+
+            msg.send(results);
+        });
+
+    }
+
     //robot.hear(/test/ig, scrapeWords);
-    robot.hear(/오늘의 단어/ig, scrapeWords);
-    robot.hear(/단어 주세요/ig, scrapeWords);
-    robot.hear(/today\'s words/ig, scrapeWords);
+    robot.hear(/오늘의 단어/i, scrapeWords);
+    robot.hear(/단어 주세요/i, scrapeWords);
+    robot.hear(/today\'s words/i, scrapeWords);
+
+    robot.hear(/예문 (.*)/i, findExample);
+    robot.hear(/example (\D*)(\d*)/i, findExample);
 
 }
