1843a8f6bdd16dc1a36128a19f7295c681b980b7
Hyunjung Kang <kanghyunjung@gmail.com>
2016-08-01 17:31:46 +0900

    can ask different day's words
    Signed-off-by: Hyunjung Kang <kanghyunjung@gmail.com>

--- scripts/wordScrape.js
+++ scripts/wordScrape.js
@@ -2,15 +2,39 @@ var jsdom = require('jsdom');
 
 module.exports = function(robot) {
 
+    //helper methods
     capitalizeFirstLetter = function(str) {
         return str[0].toUpperCase() + str.slice(1);
     }
-    scrapeWords = function(msg) {
 
+    dateToText = function(date) {
+        return "".concat(date.getFullYear(),".",date.getMonth()+1,".",date.getDate());
+    }
+
+    scrapeWords = function(msg) {
         var url = 'http://m.wordbook.naver.com/endic/today/words.nhn?targetDate=';
+        message = msg.match[0];
+
+        console.log(message);
+
+        t = new Date();
+        if(message.match(/(어제|yesterday)/)) {
+            t.setDate(t.getDate()-1);
+            url+= dateToText(t);
+        } else if(message.match(/(그저께|그제)/)) {
+            t.setDate(t.getDate()-2);
+            url+= dateToText(t);
+        }
+
+        if(msg.match.length>2 && msg.match[2].length>0) {
+            t = new Date(msg.match[2]);
+            url+= dateToText(t);
+        }
+
 
         jsdom.env(url, function(err, window) {
             var $ = require('jquery')(window);
+            console.log(url);
 
             if(err) {
                 console.log(err);
@@ -84,11 +108,12 @@ module.exports = function(robot) {
         });
 
     }
-    //test
+
     //robot.hear(/test/ig, scrapeWords);
-    robot.hear(/오늘의 단어/i, scrapeWords);
-    robot.hear(/단어 주세요/i, scrapeWords);
-    robot.hear(/today\'s words/i, scrapeWords);
+    robot.hear(/(오늘|어제|그제|그저께).*단어/i, scrapeWords);
+    robot.hear(/단어.*(주세요|나와라)/i, scrapeWords);
+    robot.hear(/(today|yesterday).*word.*/i, scrapeWords);
+    robot.hear(/([단어|word]\D*)([\d-]+)/i, scrapeWords);
 
     robot.hear(/예문 (.*)/i, findExample);
     robot.hear(/example (\D*)(\d*)/i, findExample);
